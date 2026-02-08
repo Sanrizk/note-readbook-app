091e0734ebdb30114d5aa8893a9747493405d4f2
Ikhsan Rizki <ikhsanrizki3@gmail.com>
2026-02-08 20:53:07 +0700

    create add books page

--- src/component/Modal/ModalBox/ModalForm/ModalForm.jsx
+++ src/component/Modal/ModalBox/ModalForm/ModalForm.jsx
@@ -36,57 +36,112 @@ export default function ModalForm(props) {
   //   console.log(props.radio)
   // }, [props.radio])
 
+  if (useLocation().pathname === '/books') {
+    return (
+      <form className="flex flex-col" method="POST" onSubmit={props.submit}>
+        <div className="my-1">
+          {(props.isEdit) ? (
+            <p className="flex justify-between m-2"><span>Judul:</span> <span>{props.dataBook.Title}</span></p>
+          ) : (
+            <input className="input" type="text" onChange={props.change} name="Title" placeholder="Judul Buku" value={props.dataBook.Title} />
+          )}
+        </div>
+        {
+          formInputBooks.map((input, index) => (
+            <FormInput
+              key={index}
+              onChange={props.change}
+              type={input.type}
+              name={input.name}
+              placeholder={input.placeholder}
+              value={props.dataBook[input.name] || ""}
+            />
+          ))
+        }
+        <FormSubmit />
+      </form>
+    )
+  } else {
+    return (
+      <form className="flex flex-col" method="POST" onSubmit={props.submit}>
+        <div className='flex'>
+          {formRadioReadings.map((read, index) => (
+            <FormRadio
+              key={index}
+              name='choice'
+              option={read.option}
+              changeRadio={props.changeRadio}
+              checked={props.radio === read.option}
+            />
+          ))}
+        </div>
 
-  return (
-    <form className="flex flex-col" method="POST" onSubmit={props.submit}>
-
-      {/* books */}
-      {/* <div className="my-1">
-        {(props.isEdit) ? (
-          <p className="flex justify-between m-2"><span>Judul:</span> <span>{props.dataBook.Title}</span></p>
-        ) : (
-          <input className="input" type="text" onChange={props.change} name="Title" placeholder="Judul Buku" value={props.dataBook.Title} />
-        )}
-      </div>
-      {formInputBooks.map((input, index) => (
-        <FormInput
-          key={index}
-          onChange={props.change}
-          type={input.type}
-          name={input.name}
-          placeholder={input.placeholder}
-          value={props.dataBook[input.name] || ""}
-        />
-      ))} */}
-
-      {/* readings */}
-      {/* <FormRadio name='Romawi' option='Romawi Page' checked='' change='' />
-      <FormRadio name='Romawi' option='Romawi Page' /> */}
-      <div className='flex'>
-        {formRadioReadings.map((read, index) => (
-          <FormRadio
-            key={index}
-            name='choice'
-            option={read.option}
-            changeRadio={props.changeRadio}
-            checked={props.radio === read.option}
+        <div className='flex items-center justify-around'>
+          <FormInput
+            type={props.type}
+            onChange={props.change}
+            name='ReadingNumber'
+            placeholder='Halaman'
+            value={props.dataBook.ReadingNumber}
           />
-        ))}
-      </div>
+          <p>/ {(props.radio === 'Page') ? props.dataBook.Page : props.dataBook.PageRomawi} Halaman</p>
+        </div>
 
-      <div className='flex items-center justify-around'>
-        <FormInput
-          type={props.type}
-          onChange={props.change}
-          name='ReadingNumber'
-          placeholder='Halaman'
-          value={props.dataBook.ReadingNumber}
-        />
-        <p>/ {(props.radio === 'Page') ? props.dataBook.Page : props.dataBook.PageRomawi} Halaman</p>
-      </div>
+        <FormSubmit />
+      </form>
+    )
+  }
 
-      <FormSubmit />
+  // return (
+  //   <form className="flex flex-col" method="POST" onSubmit={props.submit}>
 
-    </form>
-  )
+  //     {/* books */}
+  //     {/* <div className="my-1">
+  //       {(props.isEdit) ? (
+  //         <p className="flex justify-between m-2"><span>Judul:</span> <span>{props.dataBook.Title}</span></p>
+  //       ) : (
+  //         <input className="input" type="text" onChange={props.change} name="Title" placeholder="Judul Buku" value={props.dataBook.Title} />
+  //       )}
+  //     </div>
+  //     {formInputBooks.map((input, index) => (
+  //       <FormInput
+  //         key={index}
+  //         onChange={props.change}
+  //         type={input.type}
+  //         name={input.name}
+  //         placeholder={input.placeholder}
+  //         value={props.dataBook[input.name] || ""}
+  //       />
+  //     ))} */}
+
+  //     {/* readings */}
+  //     {/* <FormRadio name='Romawi' option='Romawi Page' checked='' change='' />
+  //     <FormRadio name='Romawi' option='Romawi Page' /> */}
+  //     <div className='flex'>
+  //       {formRadioReadings.map((read, index) => (
+  //         <FormRadio
+  //           key={index}
+  //           name='choice'
+  //           option={read.option}
+  //           changeRadio={props.changeRadio}
+  //           checked={props.radio === read.option}
+  //         />
+  //       ))}
+  //     </div>
+
+  //     <div className='flex items-center justify-around'>
+  //       <FormInput
+  //         type={props.type}
+  //         onChange={props.change}
+  //         name='ReadingNumber'
+  //         placeholder='Halaman'
+  //         value={props.dataBook.ReadingNumber}
+  //       />
+  //       <p>/ {(props.radio === 'Page') ? props.dataBook.Page : props.dataBook.PageRomawi} Halaman</p>
+  //     </div>
+
+  //     <FormSubmit />
+
+  //   </form>
+  // )
 }
